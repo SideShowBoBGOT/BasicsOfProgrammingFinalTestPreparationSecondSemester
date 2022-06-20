6202a53f3ac1e2872a2354ab8882c434ec63e622
sideshowbobgot <sideshowbobgot@gmail.com>
2022-06-20 09:23:42 +0300

    7

---
+++ 6_BinaryTree_Leaves_2/6_BinaryTree_Leaves_2/6_BinaryTree_Leaves_2.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include<list>
+using namespace std;
+class Node {
+public:
+    Node(char data) {
+        left = nullptr;
+        right = nullptr;
+        this->data = data;
+    }
+    Node* left;
+    Node* right;
+    char data;
+};
+class Tree {
+private:
+    Node* root;
+    void crawlPrint(Node* node, int level, int& branch) {
+        cout << " " << "(" << node->data << ")";
+        if (node->left != nullptr)
+            cout << "L";
+        if (node->right != nullptr)
+            cout << "R";
+        if (node->left == nullptr && node->right == nullptr)
+            cout << "N";
+        cout << "[" << branch << "]" << "{" << level << "}";
+        if (node->left != nullptr) {
+            branch++;
+            crawlPrint(node->left, level + 1, branch);
+        }
+        if (node->right != nullptr) {
+            branch++;
+            crawlPrint(node->right, level + 1, branch);
+        }
+    }
+    void crawlLeaves(Node* node, list<char>& leaves) {
+        if (node->left == nullptr && node->right == nullptr)
+            leaves.push_back(node->data);
+        if (node->left != nullptr) {
+            crawlLeaves(node->left, leaves);
+        }
+        if (node->right != nullptr) {
+            crawlLeaves(node->right, leaves);
+        }
+    }
+public:
+    Tree() {
+        this->root = nullptr;
+    }
+    list<char> getLeaves() {
+        list<char>leaves;
+        crawlLeaves(root, leaves);
+        leaves.sort();
+        return leaves;
+    }
+    void insert(char data) {
+        if (root == nullptr) {
+            root = new Node(data);
+        }
+        else {
+            bool stop = false;
+            auto cur = root;
+            while (!stop) {
+                if (data < cur->data) {
+                    if (cur->left == nullptr) {
+                        cur->left = new Node(data);
+                        stop = true;
+                    }
+                    else {
+                        cur = cur->left;
+                    }
+                }
+                if (data >= cur->data) {
+                    if (cur->right == nullptr) {
+                        cur->right = new Node(data);
+                        stop = true;
+                    }
+                    else {
+                        cur = cur->right;
+                    }
+                }
+            }
+        }
+    }
+    void print() {
+        int level = 1, branch = 0;
+        crawlPrint(root, level, branch);
+    }
+};
+int main()
+{
+    Tree t = Tree();
+    for (int i = 0; i < 10; i++)
+    {
+        t.insert(rand()%20+50);
+    }
+    t.print();
+    list<char>leaves = t.getLeaves();
+    cout <<endl<< "Leaves: " << endl;
+    for (auto i = begin(leaves); i !=end(leaves); i++)
+    {
+        cout << *i << endl;
+    }
+    return 0;
+}
+//12 minutes
---
+++ 7_BinaryTree_isBalanced/7_BinaryTree_isBalanced/7_BinaryTree_isBalanced.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+using namespace std;
+class Node {
+public:
+    Node(int d) {
+        left = nullptr;
+        right = nullptr;
+        data = d;
+    }
+    Node* left;
+    Node* right;
+    int data;
+};
+class Tree {
+private:
+    Node* root;
+    void crawlPrint(Node* node, int level, int& branch) {
+        cout << " " << "(" << node->data << ")";
+        if (node->left != nullptr)
+            cout << "L";
+        if (node->right != nullptr)
+            cout << "R";
+        if (node->right == nullptr && node->left == nullptr)
+            cout << "N";
+        cout << "[" << branch << "]" << "{" << level << "}";
+        if (node->left != nullptr) {
+            branch++;
+            crawlPrint(node->left, level + 1, branch);
+        }
+        if (node->right != nullptr) {
+            branch++;
+            crawlPrint(node->right, level + 1, branch);
+        }
+    }
+    void crawlWeight(Node* node, int& weight) {
+        if (node->left != nullptr) {
+            weight++;
+            crawlWeight(node->left, weight);
+        }
+        if (node->right != nullptr) {
+            weight++;
+            crawlWeight(node->right, weight);
+        }
+    }
+    bool crawlIsBalanced(Node* node) {
+        bool leftIsBalanced = true, rightIsBalanced = true;
+        if (node->left != nullptr)
+            leftIsBalanced = crawlIsBalanced(node->left);
+        if (node->right != nullptr)
+            rightIsBalanced = crawlIsBalanced(node->right);
+        int wleft = 0, wright = 0;
+        if (node->left != nullptr)
+            crawlWeight(node->left, wleft);
+        if (node->right != nullptr)
+            crawlWeight(node->right, wright);
+        if (abs(wleft - wright)<=1 && rightIsBalanced && leftIsBalanced)
+            return true;  
+        return false;
+    }
+public:
+    Tree() {
+        root = nullptr;
+    }
+    void print() {
+        int level = 1, branch = 0;
+        crawlPrint(root, level, branch);
+    }
+    bool isBalanced() {
+        return crawlIsBalanced(root);
+    }
+    void insert(int data) {
+        if (root == nullptr) {
+            root = new Node(data);
+        }
+        else {
+            bool stop = false;
+            auto cur = root;
+            while (!stop) {
+                if (data < cur->data) {
+                    if (cur->left == nullptr) {
+                        cur->left = new Node(data);
+                        stop = true;
+                    }
+                    else {
+                        cur = cur->left;
+                    }
+                }
+                if (data >= cur->data) {
+                    if (cur->right == nullptr) {
+                        cur->right = new Node(data);
+                        stop = true;
+                    }
+                    else {
+                        cur = cur->right;
+                    }
+                }
+            }
+        }
+    }
+};
+int main()
+{
+    Tree t = Tree();
+    t.insert(20);
+    t.insert(10);
+    t.insert(15);
+    t.insert(30);
+    t.insert(25);
+    t.insert(40);
+    
+    t.print();
+    cout <<endl<< "isBalanced: " << t.isBalanced() << endl;
+}
+//25 minutes
